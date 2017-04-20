e353e49f59e737a74e70de330c2c2c0c2b5c49f8
Alex Faunt <Alex.Faunt@thetrainline.com>
2017-04-20 17:30:14 +0100

    Use only a homepage + start server fetching

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,12 @@
 import { h } from 'preact'
 import { Switch, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import Home from './components/pages/Home'
 
 export { default as createStore } from './utils/create-store'
 
 export default ({ store }) => (
   <Provider store={store}>
-    <div>
-      <Switch>
-        <Route exact path="/" component={() => <div>Home</div>} />
-        <Route path="/about" component={() => <div>About</div>} />
-      </Switch>
-    </div>
+    <Route exact path="/" component={Home} />
   </Provider>
 )
--- server/renderer/index.jsx
+++ server/renderer/index.jsx
@@ -1,6 +1,7 @@
 import { h } from 'preact'
 import renderToString from 'preact-render-to-string'
 import { StaticRouter } from 'react-router'
+import { decode } from 'querystring'
 import App, { createStore } from '../../app'
 
 const Root = ({ store, ...rest }) => (
@@ -9,13 +10,23 @@ const Root = ({ store, ...rest }) => (
   </StaticRouter>
 )
 
+const getList = ({ origin, destination }) => {
+  return { } // TODO - this should be some kind of module that the API calls into
+}
+
+const prefetchList = (url) => {
+  const { origin, destination } = decode(url.split('?')[1])
+  return origin && destination ? getList({ origin, destination }) : null
+}
+
 export default async function (ctx) {
   const context = {}
   const store = createStore()
 
-  console.log('ctx', ctx.url)
-  // TODO - Promote routeconfig to json -> prefetch using match + ctx.url etc.
-  // Or just render twice who gives a fuck - check apollo
+  // TODO long term - route config + match + generic data prefetching
+  const data = prefetchList(ctx.url)
+  // TODO - dispatch this data into the right place in the store.
+  console.log('data', data)
 
   const html = renderToString(<Root location={ctx.url} context={context} store={store} />)
 
