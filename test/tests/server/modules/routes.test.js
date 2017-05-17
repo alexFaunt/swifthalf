4146e94fa37ce0c3385acf8364061ba831261958
Alex Faunt <Alex.Faunt@thetrainline.com>
2017-05-17 12:13:31 +0100

    first route test

---
+++ test/tests/server/modules/routes.test.js
@@ -0,0 +1,13 @@
+import suitablebreak1 from '../../../mocks/directions/suitablebreak1.json'
+import { getMidpoint } from '../../../../server/modules/routes'
+
+describe('Routes module', () => {
+  describe('#getMidpoint', () => {
+    it('returns midpoint for suitablebreak1', () => {
+      expect(getMidpoint(suitablebreak1.routes[0])).to.deep.equal({
+        lat: 51.50100699999999,
+        lng: -0.124907
+      })
+    })
+  })
+})
